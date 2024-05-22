0940fcc76785b6603fb832c5bf0e2a7055c3ba84
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-22 08:17:50 +0200

    created a index.js file to bring together the store,reducers,and actions

---
+++ model/index.js
@@ -0,0 +1,31 @@
+// Importing required functions from other modules.
+import { createStore } from './store.js';
+import { tallyReducer } from './reducers.js';
+import { addAction, subtractAction, resetAction } from './actions.js';
+
+/**
+ * Initializes and configures the store using a specific reducer.
+ * The tallyReducer is used to manage the state changes based on dispatched actions.
+ */
+const store = createStore(tallyReducer);
+
+/**
+ * Subscribes to state changes to log the updated state to the console.
+ * This function serves as a listener that triggers every time the state changes.
+ */
+store.subscribe(() => {
+    console.log('State updated:', store.getState());
+});
+
+// Logging the initial state to the console to verify that the store starts with the correct state.
+console.log('Initial state:', store.getState()); // Should log { count: 0 }
+
+//Incrementing the Counter
+store.dispatch(addAction()); // count should be 1
+store.dispatch(addAction()); // count should be 2
+
+// Decrementing the Counter
+store.dispatch(subtractAction()); // count should be 1
+
+// Resetting the Counter
+store.dispatch(resetAction()); // count should be 0
